47e4c9fd698ffc9c58ff8f376d574a89668c57bc
Danny Solivan <dark@nightconcept.net>
2022-10-07 14:41:53 -0700

    Update Windows setup for next reformat

--- desktop-setup/windows-setup-pkg.ps1
+++ desktop-setup/windows-setup-pkg.ps1
@@ -3,22 +3,36 @@ Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManage
 Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression (New-Object System.Net.WebClient).DownloadString('https://get.scoop.sh')
 
 # Install Ubuntu-20.04 LTS onto WSL
-wsl --install -d Ubuntu-20.04
+# wsl --install -d Ubuntu-20.04
 # can this be integrated into 1 command instead of two?
-wsl --set-version Ubuntu-20.04 2
+# wsl --set-version Ubuntu-20.04 2
 
 # install Windows only apps
 scoop bucket add main
 scoop bucket add versions
 scoop bucket add extras
+scoop bucket add nerd-fonts
 scoop update
 scoop install firefox-developer
 scoop install ungoogled-chromium
 scoop install vlc
 scoop install discord
 scoop install git
+scoop install gh
+scoop install github
 scoop install 7zip
 # set up 7zip context as well
 scoop install obsidian
 scoop install vscodium
+Write-Output "Add VSCodium as a context menu option by running: 'C:\Users\dark\scoop\apps\vscodium\current\install-context.reg'"
+Write-Output "# For VSCodium file associations, run 'C:\Users\dark\scoop\apps\vscodium\current\install-associations.reg'"
 scoop install hexchat
+scoop install stretchly
+scoop install notepadplusplus
+scoop install fnm
+fnm install --lts
+scoop install pnpm
+scoop install hyper
+scoop install FiraCode-NF
+scoop install FiraCode-NF-Mono
+scoop install heidisql
