7f46b778c1df6f9a6e3381afaa2b9c4770f783a3
Danny Solivan <dark@nightconcept.net>
2023-04-24 11:21:07 -0700

    Update work PC setup

--- work-setup/pc-setup.ps1
+++ work-setup/pc-setup.ps1
@@ -19,11 +19,30 @@ scoop install gitextensions
 scoop install fnm
 fnm install --lts
 scoop install pnpm
-scoop install hyper
+scoop install gpg
+scoop install github
+scoop install kitty
+scoop install 7zip
+scoop install ungoogled-chromium
+scoop install windows-terminal
 scoop install FiraCode-NF
 scoop install FiraCode-NF-Mono
 
 # Install productivity apps
 scoop install stretchly
 scoop install caffeine
-scoop install keypirinha
+scoop install logseq
+
+# PowerShell setup
+scoop install https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/oh-my-posh.json
+# Need newer version (most likely default isn't new enough) for oh-my-posh themes
+Install-Module -Force PSReadLine
+
+# Create PowerShell settings and config
+if (!(Test-Path -Path $PROFILE)) {
+  New-Item -ItemType File -Path $PROFILE -Force
+}
+$FNM = 'fnm env --use-on-cd | Out-String | Invoke-Expression'
+$OMP_CONFIG = "oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/star.omp.json' | Invoke-Expression"
+Add-Content -Path $PROFILE -Value $FNM
+Add-Content -Path $PROFILE -Value $OMP_CONFIG
