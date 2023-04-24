3f523752f8ca1c612e882e48cc14bb443b52c389
Danny Solivan <dark@nightconcept.net>
2023-04-24 11:31:06 -0700

    Update git-prompt.sh

--- .config/git/git-prompt.sh
+++ .config/git/git-prompt.sh
@@ -1,7 +1,7 @@
 # Custom prompt settings
 PROMPT_DIRTRIM=2                         # Shorten deep paths in the prompt
-PS1='\[\033]0;Git | Bash v\v | \W\007\]' # set window title
-PS1="$PS1"'\n'                           # new line
+PS1='\[\033]0;Bash]' # set window title
+#PS1="$PS1"'\n'                           # new line
 PS1="$PS1"'\[\033[0m\][\A] '        # black text, magenta, 24h time
 #PS1="$PS1"'\[\033[30;42m\]\u '          # black text, green, user
 #PS1="$PS1"'\[\033[30;42m\]@\h '          # black text, green, @host
