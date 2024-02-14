611055e0695340f91a81d818db42ec8a1b64bcf8
Danny Solivan <dark@nightconcept.net>
2024-02-13 20:38:55 -0800

    Update setup-windows-client-part2-admin.ps1

--- setup-windows-client-part2-admin.ps1
+++ setup-windows-client-part2-admin.ps1
@@ -1,3 +1,12 @@
+# Install chocolatey
+if ([bool](Get-Command -Name 'choco' -ErrorAction SilentlyContinue)) {
+  Write-Verbose "Chocolatey is already installed, skip installation." -Verbose
+}
+else {
+  Write-Verbose "Installing Chocolatey..." -Verbose
+  Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
 # Install OpenSSH Server
 if ([bool](Get-Service -Name sshd -ErrorAction SilentlyContinue)) {
   Write-Verbose "OpenSSH is already installed, skip installation." -Verbose
