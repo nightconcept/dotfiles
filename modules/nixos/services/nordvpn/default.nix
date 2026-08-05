9210a327b44cf1886d606736570092afab8e9d16
Danny Solivan <dark@nightconcept.net>
2026-08-04 22:23:41 -0700

    fix: substitute buildFHSEnv for deprecated buildFHSEnvChroot in nordvpn
    nixpkgs removed buildFHSEnvChroot; callPackage the nordvpn-flake
    derivation directly with buildFHSEnv provided instead of using its
    default package output, which still referenced the deprecated name
    and broke flake evaluation for nixosConfigurations.barrett.

--- modules/nixos/services/nordvpn/default.nix
+++ modules/nixos/services/nordvpn/default.nix
@@ -12,8 +12,10 @@
   moduleLib = import ../../../../lib/module {inherit lib;};
   inherit (moduleLib) mkBoolOpt mkOpt enabled disabled;
 
-  # Use the nordvpn package from the flake
-  nordVpnPkg = inputs.nordvpn-flake.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  # Use the nordvpn package from the flake, substituting buildFHSEnv for deprecated buildFHSEnvChroot
+  nordVpnPkg = pkgs.callPackage "${inputs.nordvpn-flake}/nordvpn.nix" {
+    buildFHSEnvChroot = pkgs.buildFHSEnv;
+  };
 in {
   options.modules.nixos.services.nordvpn = {
     enable = mkBoolOpt false "Enable the NordVPN daemon and CLI client from nordvpn-flake";
