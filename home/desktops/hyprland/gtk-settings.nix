3cf36a7d73db95de64151c0f23e68b2660dd2fe9
Danny Solivan <dark@nightconcept.net>
2026-09-04 20:13:14 -0700

    fix(hyprland): restore Tokyo Night GTK theme

--- home/desktops/hyprland/gtk-settings.nix
+++ home/desktops/hyprland/gtk-settings.nix
@@ -3,9 +3,43 @@
   lib,
   pkgs,
   ...
-}: {
+}: let
+  # nixpkgs removed this package when the unmaintained GTK 2 murrine engine was
+  # removed. Build the maintained GTK 3/4 theme directly without propagating
+  # that legacy engine.
+  tokyonightGtkTheme = pkgs.stdenvNoCC.mkDerivation {
+    pname = "tokyonight-gtk-theme";
+    version = "0-unstable-2025-10-23";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "Fausto-Korpsvart";
+      repo = "Tokyonight-GTK-Theme";
+      rev = "6c340e058e84c1975a038a8e5d1e384477225dc0";
+      hash = "sha256-7H2n9wTaW8Db1RejWK071ITV1j5KIuzfql0Tx9WT6zM=";
+    };
+
+    nativeBuildInputs = with pkgs; [
+      gnome-shell
+      sassc
+    ];
+    buildInputs = [pkgs.gnome-themes-extra];
+    dontBuild = true;
+
+    postPatch = ''
+      patchShebangs themes/install.sh
+    '';
+
+    installPhase = ''
+      runHook preInstall
+      mkdir -p "$out/share/themes"
+      cd themes
+      ./install.sh -n Tokyonight -c dark -d "$out/share/themes"
+      runHook postInstall
+    '';
+  };
+in {
   config = lib.mkIf (config.desktops.hyprland.enable or false) {
-    # Disable Stylix GTK theming to use proper Tokyo Night GTK theme
+    # Disable Stylix GTK theming to use a complete GTK 3/4 theme with readable widgets.
     stylix.targets.gtk.enable = lib.mkForce false;
 
     # GTK 3 Configuration
@@ -44,10 +78,9 @@
     gtk = {
       enable = true;
 
-      # Use proper Tokyo Night GTK theme for readable buttons
       theme = lib.mkForce {
-        name = "Tokyonight-Dark-B";
-        package = pkgs.tokyonight-gtk-theme;
+        name = "Tokyonight-Dark";
+        package = tokyonightGtkTheme;
       };
 
       iconTheme = lib.mkForce {
